3c3387ae6ca7cc88115f8b054feb643fdb78a977
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-08-05 16:50:39 +0100

    .
    .

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// const app = require('../app.js')
-// ^ abvoe line in the test file itself.
-
 const add = (num1, num2) => {
     return num1+num2;
 }
 
 module.exports = { add }
 
-// module.exports = {} at the end of our app.js file
-// require our app.js file in the test file using the require method by const app = require ('../app.js')
+
